ba0a1d45a6bb77a1c149769ff57a9e5f1ee8a642
Anthony Correia <anthony.correia333@yahoo.com>
2017-07-15 17:35:39 -0700

    messing around with components

---
+++ hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Hello extends React.Component {
+    render() {
+        return (
+            <Text> Hello from JS </Text>
+        )
+    }
+}
